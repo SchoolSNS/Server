336d930c5512d68d584e0fd2985790792838f27a
leegyurak <gi399810@gmail.com>
2020-11-03 20:22:57 +0900

    fix wrong urls

--- feedAPI/urls.py
+++ feedAPI/urls.py
@@ -3,9 +3,8 @@ from .views import CreatePostView, ReadListPostView, ReadOnePostView, UpdateDele
 
 urlpatterns = [
     path('post', CreatePostView.as_view({'post': 'create'})),
-    path('post/<int:pk>', ReadOnePostView.as_view({'get': 'retrieve'})),
     path('post/all', ReadListPostView.as_view({'get': 'list'})),
-    path('post/<int:pk>', UpdateDeletePostView.as_view({'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'})),
+    path('post/<int:pk>', UpdateDeletePostView.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'})),
     path('post/<int:post_id>/comment', CreateCommentView.as_view({'post': 'create'})),
     path('post/<int:post_id>/comments', ReadCommentView.as_view({'get': 'list'})),
     path('post/<int:post_id>/comment/<int:pk>', UpdateDeleteCommentView.as_view({'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'})),
