31ba8384653da089c375df4862f072400a827312
leegyurak <gi399810@gmail.com>
2020-11-09 17:50:49 +0900

    fixed url

--- feedAPI/urls.py
+++ feedAPI/urls.py
@@ -4,7 +4,7 @@ from .views import *
 urlpatterns = [
     path('post', CreatePostView.as_view({'post': 'create'})),
     path('post/all', ReadListPostView.as_view({'get': 'list'})),
-    path('post/school', ReadFilterBySchoolPostView({'get': 'list'})),
+    path('post/school', ReadFilterBySchoolPostView.as_view({'get': 'list'})),
     path('post/<int:pk>', UpdateDeletePostView.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
     path('post/<int:post_id>/comment', CreateCommentView.as_view({'post': 'create'})),
     path('post/<int:post_id>/comments', ReadCommentView.as_view({'get': 'list'})),
