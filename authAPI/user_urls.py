bc71ab4dd3fd40ac5c1a106ee4d838e78f5dcc06
leegyurak <gi399810@gmail.com>
2020-11-10 16:57:21 +0900

    add urls

--- authAPI/user_urls.py
+++ authAPI/user_urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import MyProfileView, UserProfileView, UsersPostView, UsersCommentView
+from .views import MyProfileView, UserProfileView, UsersPostView, UsersCommentView, UserEmailProfileView
 
 urlpatterns = [
     path('', MyProfileView.as_view({'get': 'list'})),
+    path('/email', UserEmailProfileView.as_view({'get': 'list'})),
     path('/<int:user_id>', UserProfileView.as_view({'get': 'list'})),
     path('/<int:user_id>/posts', UsersPostView.as_view({'get': 'list'})),
-    path('/<int:user_id>/comments', UsersPostView.as_view({'get': 'list'})),
+    path('/<int:user_id>/comments', UsersCommentView.as_view({'get': 'list'})),
 ]
