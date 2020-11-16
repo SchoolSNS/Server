fb9b5283ce118d58f59b6b5db36ed596690dc436
leegyurak <gi399810@gmail.com>
2020-11-16 15:26:39 +0900

    change url

--- authAPI/auth_urls.py
+++ authAPI/auth_urls.py
@@ -4,5 +4,5 @@ from .views import *
 urlpatterns = [
     path('sign-up', RegisterView.as_view()),
     path('login', LoginView.as_view()),
-    path('update/profile', ChangeProfileView.as_view()),
+    path('update', ChangeProfileView.as_view()),
 ]
